121fe9436fdada35eed1f70a2e11d8db9545e401
Jody Whitis <jodywhitis0407@gmail.com>
2020-11-27 20:03:59 -0600

    Pre-Build 1.0 - Places Page (#1)
    * Adding tables to dependency injection.
    Adding places controller and view for list of places.
    Adding link from login home.
    Updated color on top div.
    
    * Added navigation to home

---
+++ SafeSurroundings/SafeSurroundings/Controllers/PlacesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SafeSurroundings.Data.Models;
+using SafeSurroundings.Models;
+using SafeSurroundings.Data.Services;
+using SafeSurroundings.Services;
+
+namespace SafeSurroundings.Controllers
+{
+    public class PlacesController : Controller
+    {
+        InMemoryPlaceTable placeTable;
+
+        public PlacesController(InMemoryPlaceTable placeTable)
+        {
+            this.placeTable = placeTable;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<Place> places = new List<Place>();
+            places = placeTable.GetAll().ToList();
+            PlacesViewModel placesViewModel = new PlacesViewModel();
+            placesViewModel.ListofAllPlaces = places;
+            return View(placesViewModel);
+        }
+
+         
+
+    }
+}
