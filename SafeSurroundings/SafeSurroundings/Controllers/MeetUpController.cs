0e9fd1e1f0c0c31fb5735f485ab1dc1f5b176630
Jody <jodywhitis0407@gmail.com>
2021-01-31 22:37:38 -0600

    Adding MeetUp controller

---
+++ SafeSurroundings/SafeSurroundings/Controllers/MeetUpController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SafeSurroundings.Data.Models;
+using SafeSurroundings.Data.Services;
+
+namespace SafeSurroundings.Controllers
+{
+    public class MeetUpController : Controller
+    {
+        InMemoryMeetUpTable meetUpTable;
+
+        public MeetUpController(InMemoryMeetUpTable meetUpTable)
+        {
+            this.meetUpTable = meetUpTable;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
