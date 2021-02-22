b16cd340423287365997edbdce4ae989b59562f6
Jody Whitis <jodywhitis0407@gmail.com>
2021-02-21 22:32:26 -0600

    Pre build version 1.3 (#4)
    * Adding MeetUp controller
    
    * Adding meetup view model and views.
    Adding link to page from nav bar.
    
    * Adding add meetup view and Places/Meetup objects properties to view model.
    
    * Updating viewmodel to save place id.
    Adding link button to Add page.
    Updating Home link to bootstrap.
    
    * Adjusting button layout
    
    Co-authored-by: Jody <jwhitis>

---
+++ SafeSurroundings/SafeSurroundings/Controllers/MeetUpController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SafeSurroundings.Data.Models;
+using SafeSurroundings.Data.Services;
+using SafeSurroundings.Models;
+using SafeSurroundings.Services;
+
+namespace SafeSurroundings.Controllers
+{
+    public class MeetUpController : Controller
+    {
+        InMemoryMeetUpTable meetUpTable;
+        InMemoryPlaceTable placeTable;
+        public MeetUpController(InMemoryMeetUpTable meetUpTable, InMemoryPlaceTable placeTable)
+        {
+            this.meetUpTable = meetUpTable;
+            this.placeTable = placeTable;
+        }
+
+        [HttpGet]
+        [UserAuthentication]
+        public ActionResult Index()
+        {
+            List<Place> places = new List<Place>();
+            places = placeTable.GetAll().ToList();
+            List<MeetUp> meetUps = new List<MeetUp>();
+            meetUps = meetUpTable.GetAll().ToList();
+            MeetUpViewModel meetupViewModel = new MeetUpViewModel();
+
+            try
+            {
+                IEnumerable<MeetUpViewModel> meetUpViewModels =
+                    from m in meetUps
+                    join p in places on m.PlaceID equals p.ID
+                    select new MeetUpViewModel
+                    {
+                        PlaceName = p.Name,
+                        MeetTime = m.MeetTime
+                    };
+                return View(meetUpViewModels);
+            }
+            catch
+            {
+                return View("Index", "Home");
+            }
+             
+          
+        }
+
+        [HttpGet]
+        [UserAuthentication]
+        public ActionResult AddMeetUp()
+        {
+            MeetUpViewModel meetUpViewModel = new MeetUpViewModel();
+            meetUpViewModel.PlacestoMeet = placeTable.GetAll();
+            return View(meetUpViewModel);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [UserAuthentication]
+        public ActionResult AddMeetUp(MeetUpViewModel newMeetUpViewModel)
+        {
+            try
+            {
+                MeetUp newMeetup = new MeetUp();
+                newMeetup.MeetTime = newMeetUpViewModel.MeetTime;
+                newMeetup.PlaceID = newMeetUpViewModel.PlaceID;
+                newMeetup.PlaceName = placeTable.GetAll().Where(p => p.ID == newMeetup.PlaceID).Select(p => p.Name).FirstOrDefault();
+                meetUpTable.Add(newMeetup);
+                return RedirectToAction("Index","MeetUp");
+            }
+            catch{
+                return View("Error");
+            }
+        }
+    
+    }
+}
---
+++ SafeSurroundings/SafeSurroundings/Models/MeetUpViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SafeSurroundings.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SafeSurroundings.Models
+{
+    public class MeetUpViewModel
+    {
+        public string PlaceName { get; set; }
+        public DateTime MeetTime { get; set; }
+        public IEnumerable<Place> PlacestoMeet { get; set; }
+        public int PlaceID{ get; set; }
+        public MeetUp NewMeetUp { get; set; }
+     }
+}
