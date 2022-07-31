98d46aea8b5fa9eb5579caa585f60017e883a089
Jody <jodywhitis0407@gmail.com>
2022-07-31 00:07:43 -0500

    adding email sender with test. adding .Data reference to Unit test.

---
+++ SafeSurroundings/SafeSurroundings.Data/Tools/EmailTools.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.IO;
+ namespace SafeSurroundings.Data.Tools
+{
+    public class EmailTools
+    {
+        public static Boolean SendEmail(string emailSubject, List<string> senderList)
+        {
+            SmtpClient smtp = new SmtpClient();
+            MailMessage mailMessage = new MailMessage();           
+            Boolean isSent = false;
+
+            try
+            {
+                string emailTemplate = File.ReadAllText(@"EmailTemplates\EmailTemplate.html").ToString();
+                smtp.UseDefaultCredentials = Properties.Settings.Default.emailDefaultCreditials;
+                smtp.Credentials = new System.Net.NetworkCredential(Properties.Settings.Default.emailCreditials,Properties.Settings.Default.passEmailCreditials);
+                smtp.Port = Properties.Settings.Default.emailPort;
+                smtp.EnableSsl = true;
+                smtp.Host = Properties.Settings.Default.emailServer;
+
+                mailMessage.Sender = new MailAddress(Properties.Settings.Default.senderEmail);
+                mailMessage.From = new MailAddress("safeSurroundings@safe.com");
+                mailMessage.IsBodyHtml = true;
+                mailMessage.Subject = emailSubject;
+
+                foreach(string sender in senderList)
+                {
+                    mailMessage.To.Add(sender);
+                }
+
+                mailMessage.Body = emailTemplate;
+                
+                smtp.Send(mailMessage);
+                isSent = true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                isSent = false;
+            }
+            return isSent;
+        }
+    }
+}
---
+++ SafeSurroundings/SafeSurroundings.Tests/Data/Tools/EmailToolsTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SafeSurroundings.Data.Tools;
+
+namespace SafeSurroundings.Tests.Data.Tools
+{
+    [TestClass]
+    public class EmailToolsTest : EmailTools
+    {
+        [TestMethod]
+        public void TestEmailSend()
+        {
+            bool isSent = SendEmail("Test", new List<string>() { "jodywhitis0407@gmail.com" });
+            Assert.IsTrue(isSent);
+        }
+    }
+}
