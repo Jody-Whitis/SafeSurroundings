3dca3bfc33064f864e17a2d5b35e48a98f0e13d2
Jody <jodywhitis0407@gmail.com>
2022-08-21 00:33:55 -0500

    updated unit tests.

--- SafeSurroundings/SafeSurroundings.Tests/Data/Tools/EmailToolsTest.cs
+++ SafeSurroundings/SafeSurroundings.Tests/Data/Tools/EmailToolsTest.cs
@@ -11,17 +11,38 @@ namespace SafeSurroundings.Tests.Data.Tools
 {
     [TestClass]
     public class EmailToolsTest : EmailTools
-    {         
+    {
+        #region TestData
+        protected static List<string> EmailRecipients
+        {
+            get
+            {
+                return new List<string> { "projtestcred@gmail.com" };
+            }
+        }
+        protected static IEnumerable<object[]> MeetUpData()
+        {
+            List<MeetUp> listOfMeetups = new List<MeetUp>();
+            listOfMeetups.Add(new MeetUp { PlaceName = "Starbucks", MeetTime = DateTime.Now, Details = "Unit Testing" });
+            listOfMeetups.Add(new MeetUp { PlaceName = "Secret Spot", MeetTime = DateTime.Now.AddDays(1), Details = "Secret Meeting" });
+
+            return new List<object[]> {
+                new object[] {EmailRecipients,listOfMeetups}
+            };
+        }
+
+        #endregion
+
         /// <summary>
         /// Set email recipient for testing.
         /// </summary>
         /// <param name="emailBody"></param>
         /// <param name="emailRecipient"></param>
         [DataTestMethod()]
-        [DataRow("projtestcred@gmail.com","<table style='align-content:center; color:navy; font-size:20px; width:50%; border: 5px solid navy'><tr style='text-align:center'><td>1 row </td></tr></table>")]
+        [DataRow("projtestcred@gmail.com", "<table style='align-content:center; color:navy; font-size:20px; width:50%; border: 5px solid navy'><tr style='text-align:center'><td>1 row </td></tr></table>")]
         [DataRow("projtestcred@gmail.com", "<table style='align-content:center; color:navy; font-size:20px; width:50%; border: 5px solid navy'><tr style='text-align:center'><td>2 row </td></tr><tr style='text-align:center'><td>2 row </td></tr></table>")]
         [TestMethod]
-        public void TestEmailSend(string emailRecipient,string emailBody)
+        public void TestEmailSend(string emailRecipient, string emailBody)
         {
             bool isSent = SendEmail("Test", new List<string>() { emailRecipient }, emailBody);
             Assert.IsTrue(isSent);
@@ -29,20 +50,15 @@ namespace SafeSurroundings.Tests.Data.Tools
 
 
         [DataTestMethod()]
-        [DataRow("projtestcred@gmail.com", "<table style='align-content:center; border-collapse:collapse; color:navy; font-size:20px; width:50%; border: 5px solid navy;margin:0px'>$</table>","$")]
+        [DynamicData(nameof(MeetUpData), DynamicDataSourceType.Method)]
         [TestMethod]
-        public void TestSendReminder(string emailRecipient, string emailTemplate,string placeHolder)
+        public void TestSendReminder(List<string> emailRecipient, List<MeetUp> meetUps)
         {
-            List<MeetUp> listOfMeetups = new List<MeetUp>();
-            listOfMeetups.Add(new MeetUp { PlaceName = "Starbucks", MeetTime = DateTime.Now, Details = "Unit Testing" } );
-            listOfMeetups.Add(new MeetUp { PlaceName = "Secret Spot", MeetTime = DateTime.Now.AddDays(1), Details = "Secret Meeting" });
+            string emailBody = BuildRemindersText(meetUps);
 
-            string emailBody = BuildRemindersText(listOfMeetups);
-            emailTemplate = emailTemplate.Replace(placeHolder, emailBody);
+            bool isSent = SendEmail("Test", emailRecipient, emailBody);
 
-            bool isSent = SendEmail("Test", new List<string>() { emailRecipient }, emailTemplate);
-
-            Assert.IsTrue(isSent);          
+            Assert.IsTrue(isSent);
         }
     }
 }
